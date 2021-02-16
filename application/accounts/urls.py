c0dcf68a0450c14ad476298638bf7522f4a157e3
jfthisisfuller <jfthisisfuller@users.noreply.github.com>
2021-02-16 22:32:13 +0000

    Override for simple jwt views to send refresher token as http-only cookie instead of json object

--- application/accounts/urls.py
+++ application/accounts/urls.py
@@ -1,15 +1,12 @@
-from .views import CustomUserCreate, BlacklistTokenUpdateView
+from .views import CustomUserCreate, BlacklistTokenUpdateView,  CookieTokenRefreshView, CookieTokenObtainPairView
 from django.urls import path, include
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-)
 
 
 urlpatterns = [
-    path('auth/token/refresh', TokenRefreshView.as_view(), name='token_refresh'),
+    path('auth/token/refresh', CookieTokenRefreshView.as_view(), name='token_refresh'),
     path('auth/register', CustomUserCreate.as_view(), name="create_user"),
-    path('auth/login', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('auth/login', CookieTokenObtainPairView.as_view(),
+         name='token_obtain_pair'),
     path('auth/logout', BlacklistTokenUpdateView.as_view(),
          name='blacklist')
 ]
